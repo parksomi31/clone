60db9b956fceaa6d40239243d97ffb66a4458c21
BIT <parksomi31>
2024-06-28 14:45:31 +0900

    frist

---
+++ TeamProject/src/main/java/kr/soft/study/controller/BController2.java
@@ -0,0 +1,14 @@
+package kr.soft.study.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Handles requests for the application home page.
+ */
+
+@Controller // 이 클래스가 Spring MVC의 컨트롤러 역할
+public class BController2 {
+
+	
+
+}
